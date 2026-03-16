f822c5c853ed7673e160099ecffa8c63a48652b4
T S Sainiranjan <tssainiranjan@gmail.com>
2026-03-16 18:18:22 +0530

    setup github pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: "/shuffles/",
   plugins: [
     TanStackRouterVite({
       routesDirectory: './src/routes',
